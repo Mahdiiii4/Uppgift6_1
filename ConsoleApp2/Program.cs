2a305f90faab1626a90144060a6b3c1458641702
Alexseptt100 <alan03002@utb.vaxjo.se>
2023-02-10 10:05:51 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tal(2, 5, 6);
+        }
+        static void tal(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            Console.WriteLine("Den summa är" + summa + ".");
+        }
+    }
+}
