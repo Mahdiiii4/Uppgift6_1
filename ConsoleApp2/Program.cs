65aa4a0a2e9139edadf579bff11f34142eb1a7cb
Mahdiiii4 <111962604+Mahdiiii4@users.noreply.github.com>
2023-02-10 11:00:47 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
+using System;
 namespace uppgift6_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            tal(2, 5, 6);
+            Console.WriteLine("Skriv ner var en nummer på var sin rad");
+            int nummer = int.Parse(Console.ReadLine());
+            int nummer2 = int.Parse(Console.ReadLine());
+            int nummer3 = int.Parse(Console.ReadLine());
+            tal(nummer, nummer2, nummer3);
         }
         static void tal(int tal1, int tal2, int tal3)
         {
